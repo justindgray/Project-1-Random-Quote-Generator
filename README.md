98819d7d06302c3d3d7cc2dd0438b3f15783c9ad
justindgray <justindgray@gmail.com>
2021-07-27 20:06:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Random-Quote
+ Treehouse Random Quote Generator
---
+++ quotes.js
@@ -0,0 +1,52 @@
+var quotes = [
+    {
+        quote: '"Frankly, my dear, I don\'t give a damn."',
+        source: 'Gone with the Wind',
+        year: '1939'
+    },
+    {
+        quote: '"I\'m gonna make him an offer he can\'t refuse."',
+        source: 'The Godfather',
+        year: '1972'
+    },
+    {
+        quote: '"You don\'t understand! I coulda had class. I coulda been a contender. I could\'ve been sombody, instead of a bum, which is what I am."',
+        source: 'On the Waterfront',
+        year: '1954'
+    },
+    {
+        quote: '"Toto, I\'ve a feeling we\'re not in Kansas anymore."',
+        source: 'The Wizard of Oz',
+        year: '1954'
+    },
+    {
+        quote: '"Here\'s looking at you, kid."',
+        source: 'Casablanca',
+        year: '1942'
+    },
+    {
+        quote: '"Go ahead, make my day."',
+        source: 'Sudden Impact',
+        year: '1983'
+    },
+    {
+        quote: '"All right, Mr. DeMille, I\'m ready for my close-up."',
+        source: 'Sunset Blvd.',
+        year: '1950'
+    },
+    {
+        quote: '"May the Force be with you."',
+        source: 'Star Wars',
+        year: '1977'
+    },
+    {
+        quote: '"Fasten your seatbelts. It\'s going to be a bumpy night."',
+        source: 'All About Eve',
+        year: '1950'
+    },
+    {
+        quote: '"You talking to me?"',
+        source: 'Taxi Driver',
+        year: '1976'
+    }
+]
